c18c8fc8f7570f7f8af6fa7cdf2cc1555491aecf
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-08-30 13:55:37 -0700

    improved yaml parser

--- yaml/parse_test.go
+++ yaml/parse_test.go
@@ -1,1 +1,144 @@
 package yaml
+
+import (
+	"testing"
+
+	"github.com/franela/goblin"
+)
+
+func TestParse(t *testing.T) {
+
+	conf, err := ParseString(sample)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	g := goblin.Goblin(t)
+	g.Describe("Parse Yaml", func() {
+
+		g.It("Should parse images", func() {
+			g.Assert(conf.Clone.Image).Equal("git")
+		})
+
+		g.It("Should parse image force-pull", func() {
+			g.Assert(conf.Clone.Pull).Equal(true)
+		})
+
+		g.It("Should parse variable arguments", func() {
+			g.Assert(conf.Clone.Vargs["path"]).Equal("github.com/octocat/hello-world")
+		})
+
+		g.It("Should parse build image", func() {
+			g.Assert(conf.Build.Image).Equal("golang")
+		})
+
+		g.It("Should parse build commands", func() {
+			g.Assert(conf.Build.Commands).Equal([]string{"go build", "go test"})
+		})
+
+		g.It("Should parse volume configuration", func() {
+			g.Assert(conf.Build.Volumes).Equal([]string{"/tmp/volumes"})
+		})
+
+		g.It("Should parse network configuration", func() {
+			g.Assert(conf.Build.Net).Equal("bridge")
+		})
+
+		g.It("Should parse environment variable map", func() {
+			g.Assert(conf.Clone.Environment.Slice()).Equal(
+				[]string{"GIT_DIR=.git"},
+			)
+		})
+
+		g.It("Should parse environment variable slice", func() {
+			g.Assert(conf.Build.Environment.Slice()).Equal(
+				[]string{"GO15VENDOREXPERIMENT=1"},
+			)
+		})
+
+		g.It("Should parse docker command slice", func() {
+			g.Assert(conf.Compose.Slice()[0].Command.Slice()).Equal(
+				[]string{
+					"redis-server",
+					"/usr/local/etc/redis/redis.conf",
+					"--appendonly",
+					"yes",
+				},
+			)
+		})
+
+		g.It("Should parse docker command string", func() {
+			g.Assert(conf.Compose.Slice()[1].Command.Slice()).Equal(
+				[]string{
+					"--storageEngine",
+					"wiredTiger",
+				},
+			)
+		})
+
+		g.It("Should allow multiple plugins of same type", func() {
+			s := conf.Deploy.Slice()
+			g.Assert(s[0].Image).Equal("heroku")
+			g.Assert(s[1].Image).Equal("heroku")
+		})
+
+		g.It("Should maintain plugin ordering", func() {
+			s := conf.Deploy.Slice()
+			g.Assert(s[0].Vargs["app"]).Equal("foo.com")
+			g.Assert(s[1].Vargs["app"]).Equal("dev.foo.com")
+		})
+
+		g.It("Should parse plugin filters", func() {
+			s := conf.Deploy.Slice()
+			g.Assert(s[0].Filter.Branch).Equal("master")
+			g.Assert(s[1].Filter.Repo).Equal("octocat/helloworld")
+			g.Assert(s[1].Filter.Matrix).Equal(map[string]string{"go_version": "1.5"})
+		})
+	})
+}
+
+var sample = `
+clone:
+  image: git
+  pull: true
+  path: github.com/octocat/hello-world
+  environment:
+    GIT_DIR: .git
+
+build:
+  image: golang
+  environment:
+    - GO15VENDOREXPERIMENT=1
+  commands:
+    - go build
+    - go test
+  volumes:
+    - /tmp/volumes
+  net: bridge
+  privileged: true
+
+compose:
+  redis:
+    image: library/redis
+    command: redis-server /usr/local/etc/redis/redis.conf --appendonly yes
+
+  mongo:
+    image: library/mongo
+    command:
+      - --storageEngine
+      - wiredTiger
+
+deploy:
+  heroku:
+    app: foo.com
+    when:
+      branch: master
+  heroku:
+    app: dev.foo.com
+    when:
+      repo: octocat/helloworld
+      branch: somebranch
+      matrix:
+        go_version: 1.5
+`
--- yaml/types.go
+++ yaml/types.go
@@ -1,1 +1,61 @@
 package yaml
+
+// Config is a typed representation of the
+// Yaml configuration file.
+type Config struct {
+	Cache *Plugin
+	Clone *Plugin
+	Build *Build
+
+	Compose Containerslice
+	Publish Pluginslice
+	Deploy  Pluginslice
+	Notify  Pluginslice
+}
+
+// Container is a typed representation of a
+// docker step in the Yaml configuration file.
+type Container struct {
+	Image       string
+	Pull        bool
+	Privileged  bool
+	Environment MapEqualSlice
+	Entrypoint  Command
+	Command     Command
+	Volumes     []string
+	Net         string
+}
+
+// Build is a typed representation of the build
+// step in the Yaml configuration file.
+type Build struct {
+	Container `yaml:",inline"`
+
+	Commands []string
+}
+
+// Plugin is a typed representation of a
+// docker plugin step in the Yaml configuration
+// file.
+type Plugin struct {
+	Container `yaml:",inline"`
+
+	Vargs  Vargs  `yaml:",inline"`
+	Filter Filter `yaml:"when"`
+}
+
+// Vargs holds unstructured arguments, specific
+// to the plugin, that are used at runtime when
+// executing the plugin.
+type Vargs map[string]interface{}
+
+// Filter is a typed representation of filters
+// used at runtime to decide if a particular
+// plugin should be executed or skipped.
+type Filter struct {
+	Repo    string
+	Branch  string
+	Success string
+	Failure string
+	Matrix  map[string]string
+}
